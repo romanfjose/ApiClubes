986c44b20dd52866e6d2873065c510b9824215b9
JoseRoman <48696141+romanfjose@users.noreply.github.com>
2021-12-11 19:41:00 -0300

    Add files via upload

---
+++ src/test/java/com/example/newteamapi/NewTeamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newteamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTeamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
